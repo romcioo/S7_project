c6f564b8b4540b0b4b0656206ba38d87b5a564e7
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2020-11-04 09:56:04 +0100

    euler rotate

---
+++ matlab/rotateE.m
@@ -0,0 +1,10 @@
+function rotPoints = rotateE(angles,points,reverse)
+rotm = eul2rotm(angles,'XYZ');
+if reverse
+    rotm = rotm^-1;
+end
+
+rotPoints = zeros(size(points));
+for i = 1:size(points,1)
+    rotPoints(i,:) = (rotm*points(i,:)')';
+end
