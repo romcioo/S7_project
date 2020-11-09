0abc03a9a4b5a942dab515b5604356816f960b31
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2020-11-09 12:24:43 +0100

    local trim

--- local_planner.cpp
+++ local_planner.cpp
@@ -1,3 +1,4 @@
+/*Include*/
 #include "ros/ros.h"
 #include "std_msgs/Bool.h"
 #include "geometry_msgs/Quaternion.h"
@@ -7,8 +8,7 @@
 #include <iostream>
 #include <random>
 #include <math.h>
-// #include <time>
-// #include "webots/Robot.h"
+/*!Include!*/
 
 /*Declare*/
 void objectiveCallback(const geometry_msgs::Vector3::ConstPtr& msg);
@@ -23,6 +23,7 @@ std_msgs::Bool reachedQ(geometry_msgs::Vector3 objective, geometry_msgs::Vector3
 void head(double initial[], geometry_msgs::Vector3Stamped position, geometry_msgs::Vector3 &heading, ros::Publisher diff_pub, ros::Publisher diff_rot_pub);
 void multiply(double trans[3][3], double point[3], double result[3]);
 void substract(geometry_msgs::Vector3 vec1, geometry_msgs::Vector3 vec2, double result[3]);
+/*!Declare!*/
 
 
 int main(int argc, char **argv) {
@@ -31,9 +32,6 @@ int main(int argc, char **argv) {
   ros::Publisher heading_pub = n.advertise<geometry_msgs::Vector3>("heading", 1000);
   ros::Publisher visited_pub = n.advertise<geometry_msgs::Vector3Stamped>("path", 1000);
   ros::Publisher reached_pub = n.advertise<std_msgs::Bool>("reachPoint", 1000);
-  // ros::Publisher position_shifted_pub = n.advertise<geometry_msgs::Vector3>("robot_pose_shifted", 1000);
-  ros::Publisher diff_pub = n.advertise<geometry_msgs::Vector3>("diff", 100);
-  ros::Publisher diff_rot_pub = n.advertise<geometry_msgs::Vector3>("diff_rot", 100);
 
   ros::Subscriber objective_sub = n.subscribe("goal", 1000, objectiveCallback);
   ros::Subscriber global_position_sub = n.subscribe("robot_pose", 1000, globalCallback);
@@ -41,12 +39,6 @@ int main(int argc, char **argv) {
 
   ros::Rate loop_rate(1);
 
-  // Initialize
-  // global_position.vector.x = double(0);
-  // global_position.vector.y = double(0);
-  // global_position.vector.z = double(0);
-
-  // geometry_msgs::Quaternion heading_msg;
   geometry_msgs::Vector3Stamped position;
   std_msgs::Bool reached;
   geometry_msgs::Vector3 heading = geometry_msgs::Vector3();
@@ -77,19 +69,6 @@ void objectiveCallback(const geometry_msgs::Vector3::ConstPtr& msg){
 }
 
 void globalCallback(const geometry_msgs::Vector3Stamped::ConstPtr& msg) {
-  /*geometry_msgs::Vector3 vector = msg->vector;
-  std_msgs::Header header = msg->header;
-
-  double posx = vector.x;
-  double posy = vector.y;
-  double posz = vector.z;
-
-  //global_position = geometry_msgs::Vector3(posx,posy,posz);
-  //geometry_msgs::Vector3
-
-  global_position.vector.x = posx;
-  global_position.vector.y = posy;
-  global_position.vector.z = posz;*/
   global_position = *msg;
 }
 
@@ -98,14 +77,7 @@ void imuCallback(const geometry_msgs::Vector3::ConstPtr& msg) {
 }
 
 geometry_msgs::Vector3Stamped addNoise(geometry_msgs::Vector3Stamped point) {
-  // double stddev;
-  // const double mean = 0.0;
-  // std::default_random_engine generator;
-
   geometry_msgs::Vector3Stamped vector = point;
-  // ros::Time time = point->header.stamp;
-
-  // std::normal_distribution<double> dist(mean, stddev);
 
   return vector;
 }
@@ -132,13 +104,6 @@ void head(double initial[], geometry_msgs::Vector3Stamped position, geometry_msg
   double yaw = imu.y + initial[1];
   double pitch = imu.z + initial[2];
 
-  // double rotation_matrix[3][3] = {{cos(yaw)*cos(pitch),
-  //   sin(roll)*sin(yaw)*cos(pitch)-cos(roll)*sin(pitch),
-  //   cos(roll)*sin(yaw)*cos(pitch)+sin(roll)*sin(pitch)},
-  //   {cos(yaw)*cos(pitch),
-  //   sin(roll)*sin(yaw)*sin(pitch)+cos(roll)*cos(pitch),
-  //   cos(roll)*sin(yaw)*sin(pitch)-sin(roll)*cos(pitch)},
-  //   {-sin(yaw), sin(roll)*cos(yaw), sin(roll)*cos(yaw)}};
   double rotation_matrix[3][3] = {{cos(roll)*cos(pitch) - sin(roll)*cos(yaw)*sin(pitch),
     -cos(roll)*sin(pitch) - sin(roll)*cos(yaw)*cos(pitch),
     sin(roll)*sin(yaw)},
@@ -149,15 +114,7 @@ void head(double initial[], geometry_msgs::Vector3Stamped position, geometry_msg
 
   substract(objective, position.vector, diff);
   multiply(rotation_matrix, diff, diff_rot);
-  diffV.x = diff[0];
-  diffV.y = diff[1];
-  diffV.z = diff[2];
-  diff_rotV.x = diff_rot[0];
-  diff_rotV.y = diff_rot[1];
-  diff_rotV.z = diff_rot[2];
-  // ROS_INFO("initial: [%s]\nfinal: [%s]", diff, diff_rot);
-  diff_pub.publish(diffV);
-  diff_rot_pub.publish(diff_rotV);
+
   heading.y = atan(diff_rot[1]/diff_rot[0]);
   heading.x = std::sqrt(pow(diff_rot[0],2) + pow(diff_rot[1],2) + pow(diff_rot[2],2));
 }
