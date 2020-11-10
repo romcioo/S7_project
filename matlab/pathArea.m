744a5fe37a2920fbbabb93e3d01c1298e8f11ff0
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2020-11-10 23:33:12 +0100

    a bit better

--- matlab/pathArea.m
+++ matlab/pathArea.m
@@ -7,12 +7,12 @@ area = zeros(1,size(T,1)/2);
 for i = 1:size(T,1)/2
     tri = T(2*i-1:2*i,:);
     rect1 = getRect(tri);
-    for j = 1:2
-        triangle = P(tri(j,:),:);
-        preA = triangArea(triangle);
-    end
     
     if i == 1
+        for j = 1:2
+            triangle = P(tri(j,:),:);
+            preA = triangArea(triangle);
+        end
         area(i) = preA;
     else
         eulIndex = max(max(tri));
@@ -27,9 +27,9 @@ for i = 1:size(T,1)/2
             rect2 = T(2*j-1:2*j,:);
             rect2 = getRect(rect2);
             po2 = PR(rect2,:);
-            poly2 = polyshape(po2(:,1),po2(:,2));
             [av2,rng2] = averageZ(po2);
             if abs(rng1-rng2) < .5 && abs(av1-av2) < .5
+                poly2 = polyshape(po2(:,1),po2(:,2));
                 poly1 = subtract(poly1,poly2);
             end
             if poly1.area == 0
