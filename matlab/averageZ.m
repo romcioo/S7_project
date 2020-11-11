01df7d5417ef2ae419a1de3814fd8e9e45d4fe9f
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2020-11-11 10:57:50 +0100

    average comment

--- matlab/averageZ.m
+++ matlab/averageZ.m
@@ -1,4 +1,4 @@
 function [avZ,range] = averageZ(rect)
-z1 = rect(:,3);
-avZ = mean(z1,1);
-range = max(rect(:,3)) - min(rect(:,3));
+z1 = rect(:,3); % get all z
+avZ = mean(z1,1); % average z
+range = max(z1) - min(z1); % range of z
