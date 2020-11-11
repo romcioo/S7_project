04134f377bb5f440b93c0d3a6c2e9ce6bd3d3dda
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2020-11-11 10:50:51 +0100

    getRect comm

--- matlab/getRect.m
+++ matlab/getRect.m
@@ -2,13 +2,13 @@ function rect = getRect(T)
 rect = zeros(1,4);
 long = reshape(T,[1 6]); % flat the T
 mIn = min(long); % get the minimum value (first point)
-if T(2,1) == mIn % if the first number is the minimum
+if T(2,1) == mIn % if the first number of the second triangle is the minimum
     rect(1:2) = [mIn+1 mIn]; % the first two points
 else
     rect(1:2) = [mIn mIn+1];
 end
-if T(1,3) == mIn+2
-    rect(3:4) = [mIn+3 mIn+2];
+if T(1,3) == mIn+2 % if the last number of the first triangle is the the third number
+    rect(3:4) = [mIn+3 mIn+2]; % the last two numbers
 else
     rect(3:4) = [mIn+2 mIn+3];
 end
