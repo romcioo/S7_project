e1e8b9a81cb3792cb1061d2afee9db9c475590bc
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2020-11-11 10:48:40 +0100

    getRect comment

--- matlab/getRect.m
+++ matlab/getRect.m
@@ -1,9 +1,9 @@
 function rect = getRect(T)
 rect = zeros(1,4);
-long = reshape(T,[1 6]);
-mIn = min(long);
-if T(2,1) == mIn
-    rect(1:2) = [mIn+1 mIn];
+long = reshape(T,[1 6]); % flat the T
+mIn = min(long); % get the minimum value (first point)
+if T(2,1) == mIn % if the first number is the minimum
+    rect(1:2) = [mIn+1 mIn]; % the first two points
 else
     rect(1:2) = [mIn mIn+1];
 end
