8df7f59b16ba3c3f7e2f318fd4c6d9c7430e703a
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2020-11-11 10:07:57 +0100

    getTriangulation comment

--- matlab/getTriangulation.m
+++ matlab/getTriangulation.m
@@ -1,10 +1,10 @@
 function T = getTriangulation(p,num)
-side1 = p(1:2,1:2);
+side1 = p(1:2,1:2); % get the known sides
 side2 = p(3:4,1:2);
-L11 = getLine(side1(1,:),side2(1,:));
+L11 = getLine(side1(1,:),side2(1,:)); % get the wanted lines
 L22 = getLine(side1(2,:),side2(2,:));
-[~,~,C1] = crossLines(L11,L22,[side1(1,1) side2(1,1);side1(2,1) side2(2,1)]);
-if C1
+[~,~,C1] = crossLines(L11,L22,[side1(1,1) side2(1,1);side1(2,1) side2(2,1)]); % get the cross lines
+if C1 % if the looking line if the one crossing
     T = [num(1,1) num(1,2) num(2,1);
         num(1,1) num(2,1) num(2,2)];
 else
