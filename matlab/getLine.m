ae3ae95a49381b7cbb6d5931e346438dfc2a8ae4
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2020-11-11 11:23:28 +0100

    getLine comment

--- matlab/getLine.m
+++ matlab/getLine.m
@@ -1,6 +1,6 @@
 function line = getLine(p1,p2)
-if p1(1) == p2(1)
-    line = [NaN p1(1)];
+if p1(1) == p2(1) % if it is a vertical line
+    line = [NaN p1(1)]; % line(2) is x cross line
 else
-    line = polyfit([p1(1) p2(1)],[p1(2) p2(2)],1);
+    line = polyfit([p1(1) p2(1)],[p1(2) p2(2)],1); 
 end
