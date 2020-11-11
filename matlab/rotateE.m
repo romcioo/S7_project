5cda8f92da6c8d44d63591b8795514cad50f32f8
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2020-11-11 09:57:53 +0100

    rotateE comment

--- matlab/rotateE.m
+++ matlab/rotateE.m
@@ -1,14 +1,11 @@
 function rotPoints = rotateE(angles,points,reverse)
-% disp(angles)
-% disp(points)
-angles = -angles;
-rotm = eul2rotm(angles,'XYZ');
-if reverse
-    rotm = rotm^-1;
+angles = -angles; % set negative angles to make it work
+rotm = eul2rotm(angles,'XYZ'); % get the rotaiton matrix
+if reverse % if its necessary to go back to the global
+    rotm = rotm^-1; % reverse
 end
-% assignin('base','rotm',rotm)
 
 rotPoints = zeros(size(points));
-for i = 1:size(points,1)
-    rotPoints(i,:) = (rotm*points(i,:)')';
+for i = 1:size(points,1) % for each point
+    rotPoints(i,:) = (rotm*points(i,:)')'; % convert points
 end
