75baaa874ca30eff669c04991df71f7d06ef06d8
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2020-11-27 09:28:51 +0100

    comment trim

--- matlab/rotateE.m
+++ matlab/rotateE.m
@@ -1,14 +1,4 @@
 function rotPoints = rotateE(mat,points,reverse)
-% mat = -mat; % set negative angles to make it work
-% rotm = eul2rotm(mat,'XYZ'); % get the rotation matrix
-% if reverse % if its necessary to go back to the global
-%     rotm = rotm^-1; % reverse
-% end
-% 
-% rotPoints = zeros(size(points));
-% for i = 1:size(points,1) % for each point
-%     rotPoints(i,:) = (rotm*points(i,:)')'; % convert points
-% end
 if reverse
     mat = mat';
 end
--- matlab/widthPath.m
+++ matlab/widthPath.m
@@ -10,25 +10,18 @@ for i = 2:size(points,1) % go through all points
     p1 = points(i,:); % point to look at
     p2 = points(i-1,:); % point behind it
     vect = p1 - p2; % vector p2-p1
-%     vect = (rotation*vect')';
     vect = rotateE(rotation,vect,0); % put it to robots plane
     angle = atan(vect(3)/vect(1)); % get angle on x-y plane
     shiftD = shift*[cos(angle) 0 sin(angle)]; % get the vector direction coordinates to make the path width
     pT = zeros(4,3);
-%     eui = mat(i,:); % get the angles of the points
     if i == 2 % for the first iteration
-%         P(1,:) = rotateE(eui,(rP*shiftD')',1) + p2; % get the 2 firts points in global coordinates
-%         P(2,:) = rotateE(eui,(rN*shiftD')',1) + p2;
         P(1,:) = rotateE(rotation,rP*shiftD,1) + p2;
         P(2,:) = rotateE(rotation,rN*shiftD,1) + p2;
     end
-%     P(2*i-1,:) = rotateE(eui,(rP*shiftD')',1) + p1; % points in global coordinates
-%     P(2*i,:) = rotateE(eui,(rN*shiftD')',1) + p1;
     P(2*i-1,:) = rotateE(rotation,rP*shiftD,1) + p1;
     P(2*i,:) = rotateE(rotation,rN*shiftD,1) + p1;
     n = 2*(i-1);
     for j = 1:4
-%         pT(j,:) = rotateE(eui,P(2*i-(4-j),:),0); % get the points on the robot coordinates
         pT(j,:) = rotateE(rotation,P(2*i-(4-j),:),0); % get the points on the robot coordinates
     end
     nums = [n-1 n;n+1 n+2];
