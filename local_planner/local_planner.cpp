543e3334d00d5dd8adc7678dd197f093562b8b20
9colat <34935989+9colat@users.noreply.github.com>
2020-12-01 11:53:12 +0100

    Add files via upload

--- local_planner/local_planner.cpp
+++ local_planner/local_planner.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <random>
 #include <math.h>
+#include <stdlib.h>
+#include <algorithm>
 /*!Include!*/
 
 /*Declare*/
@@ -17,10 +19,15 @@ void globalCallback(const geometry_msgs::Quaternion::ConstPtr& msg);
 extern geometry_msgs::Quaternion global_position = geometry_msgs::Quaternion();
 void imuCallback(const geometry_msgs::Vector3::ConstPtr& msg);
 extern geometry_msgs::Vector3 imu = geometry_msgs::Vector3();
+void row1Callback(const geometry_msgs::Vector3::ConstPtr& msg);
+void row2Callback(const geometry_msgs::Vector3::ConstPtr& msg);
+void row3Callback(const geometry_msgs::Vector3::ConstPtr& msg);
+extern double rotationMatrix[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+extern double rM[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
 
 // geometry_msgs::Vector3Stamped addNoise(geometry_msgs::Vector3Stamped point);
 std_msgs::Bool reachedQ(geometry_msgs::Vector3 objective, geometry_msgs::Quaternion position);
-void head(geometry_msgs::Quaternion position, geometry_msgs::Vector3 &heading);
+void head(double initial[], geometry_msgs::Quaternion position, geometry_msgs::Vector3 &heading);
 void multiply(double trans[3][3], double point[3], double result[3]);
 void subtract(geometry_msgs::Vector3 vec1, geometry_msgs::Vector3 vec2, double result[3]);
 /*!Declare!*/
@@ -32,27 +39,39 @@ int main(int argc, char **argv) {
 
   /*Publishers*/
   ros::Publisher heading_pub = n.advertise<geometry_msgs::Vector3>("heading", 1000);
+  // ros::Publisher visited_pub = n.advertise<geometry_msgs::Vector3Stamped>("path", 1000);
   ros::Publisher reached_pub = n.advertise<std_msgs::Bool>("reachPoint", 1);
+  // ros::Publisher absolute_imu_pub = n.advertise<geometry_msgs::Vector3>("absoluteIMU", 1000);
   /*!Publishers!*/
 
   /*Subscribers*/
   ros::Subscriber objective_sub = n.subscribe("goal", 1000, objectiveCallback);
   ros::Subscriber global_position_sub = n.subscribe("path", 1000, globalCallback);
   ros::Subscriber imu_sub = n.subscribe("imuValues", 1000, imuCallback);
+  ros::Subscriber row1_sub = n.subscribe("row1",2,row1Callback);
+  ros::Subscriber row2_sub = n.subscribe("row2",2,row2Callback);
+  ros::Subscriber row3_sub = n.subscribe("row3",2,row3Callback);
   /*!Subscribers!*/
 
-  ros::Rate loop_rate(10); // time delay in Hz (10Hz)
+  ros::Rate loop_rate(50); // time delay in Hz (10Hz)
 
   /*Declare*/
+  // geometry_msgs::Vector3Stamped position;
   std_msgs::Bool reached;
   geometry_msgs::Vector3 heading = geometry_msgs::Vector3();
+  double initial_rotation[3] = {0,0,0};
+  //double rM[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+  //double rotationMatrix[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
   /*!Declare!*/
 
   while (ros::ok()) {
+    // position = addNoise(global_position);
+    // visited_pub.publish(global_position);
+
     reached = reachedQ(objective, global_position);
     reached_pub.publish(reached);
 
-    head(global_position, heading);
+    head(initial_rotation, global_position, heading);
     heading_pub.publish(heading);
 
     ros::spinOnce();
@@ -77,41 +96,69 @@ void globalCallback(const geometry_msgs::Quaternion::ConstPtr& msg) {
 void imuCallback(const geometry_msgs::Vector3::ConstPtr& msg) {
   imu = *msg;
 }
+
+void row1Callback(const geometry_msgs::Vector3::ConstPtr& msg) {
+	geometry_msgs::Vector3 v = *msg;
+	rM[0][0] = v.x;
+	rM[0][1] = v.y;
+	rM[0][2] = v.z;
+}
+
+void row2Callback(const geometry_msgs::Vector3::ConstPtr& msg) {
+	geometry_msgs::Vector3 v = *msg;
+	rM[1][0] = v.x;
+	rM[1][1] = v.y;
+	rM[1][2] = v.z;
+}
+
+void row3Callback(const geometry_msgs::Vector3::ConstPtr& msg) {
+	geometry_msgs::Vector3 v = *msg;
+	rM[2][0] = v.x;
+	rM[2][1] = v.y;
+	rM[2][2] = v.z;
+
+	//rotationMatrix[][] = rM[][];
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			rotationMatrix[i][j] = rM[i][j];
+		}
+	}
+}
 /*!Get ROS information into main!*/
 
+// geometry_msgs::Vector3Stamped addNoise(geometry_msgs::Vector3Stamped point) { // add noise to the position
+//   geometry_msgs::Vector3Stamped vector = point;
+//
+//   return vector;
+// }
+
 std_msgs::Bool reachedQ(geometry_msgs::Vector3 objective, geometry_msgs::Quaternion position) { // Check if the robot has arrived to the current goal
-  float error = .25; // radious to accept reached
+  double error = .25; // radious to accept reached
+	double rotated[3];
 
   /*Difference on each coordinate*/
-  float x = objective.x - position.x;
-  float y = objective.y - position.y;
+  double x = objective.x - position.x;
+  double y = objective.y - position.y;
+  double z = objective.z - position.z;
+	double global_dist[3] = {x, y, z};
+
+	multiply(rotationMatrix, global_dist, rotated);
   /*!Difference on each coordinate!*/
 
   std_msgs::Bool reached = std_msgs::Bool();
-  reached.data = error > std::sqrt(pow(x,2) + pow(y,2));
+  reached.data = error > std::sqrt(pow(rotated[0],2) + pow(rotated[2],2));
   return reached;
 }
 
-void head(geometry_msgs::Quaternion position, geometry_msgs::Vector3 &heading) { // calculate the turning angle and distance to the goal
+void head(double initial[], geometry_msgs::Quaternion position, geometry_msgs::Vector3 &heading) { // calculate the turning angle and distance to the goal
   /*Declare*/
   double diff[3];
   double diff_rot[3];
   geometry_msgs::Vector3 pos;
+  double angle;
   /*!Declare!*/
 
-  double roll = imu.x;
-  double yaw = imu.y;
-  double pitch = imu.z;
-
-  /*Matrix to rotate on Euler angles*/
-  double rotation_matrix[3][3] = {{cos(roll)*cos(pitch) - sin(roll)*cos(yaw)*sin(pitch),
-    -cos(roll)*sin(pitch) - sin(roll)*cos(yaw)*cos(pitch),
-    sin(roll)*sin(yaw)},
-    {sin(roll)*cos(pitch) + cos(roll)*cos(yaw)*sin(pitch),
-    -sin(roll)*sin(pitch) + cos(roll)*cos(yaw)*cos(pitch),
-    -cos(roll)*sin(yaw)},
-    {sin(yaw)*sin(pitch), sin(yaw)*cos(pitch), cos(yaw)}};
-  /*!Matrix to rotate on Euler angles!*/
+  geometry_msgs::Vector3 pub_imu;
 
   pos = geometry_msgs::Vector3();
   pos.x = position.x;
@@ -120,11 +167,16 @@ void head(geometry_msgs::Quaternion position, geometry_msgs::Vector3 &heading) {
 
   /*Get vector rotated*/
   subtract(objective, pos, diff);
-  multiply(rotation_matrix, diff, diff_rot);
+  multiply(rotationMatrix, diff, diff_rot);
   /*Get vector rotated*/
 
-  heading.y = atan(diff_rot[1]/diff_rot[0]); // angle
-  heading.x = std::sqrt(pow(diff_rot[0],2) + pow(diff_rot[1],2) + pow(diff_rot[2],2));
+  angle = atan2(-diff_rot[2],diff_rot[0]); // angle
+  /*angle -= pi;
+  if (angle < pi) {
+  	angle += 2*pi;
+  }*/
+  heading.y = angle;
+  heading.x = std::sqrt(pow(diff_rot[0],2) + pow(diff_rot[2],2));
 }
 
 void multiply(double trans[3][3], double point[3], double result[3]) { // Multiply a [3x3] matrix by [3x1]
