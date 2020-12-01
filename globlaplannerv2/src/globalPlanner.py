4ba6682222bb159a42dc268b59a3efb23d69b35b
9colat <34935989+9colat@users.noreply.github.com>
2020-12-01 11:51:28 +0100

    Add files via upload

--- globlaplannerv2/src/globalPlanner.py
+++ globlaplannerv2/src/globalPlanner.py
@@ -14,7 +14,7 @@ from geometry_msgs.msg import Vector3Stamped
 from geometry_msgs.msg import Quaternion
 import numpy as np
 
-edge = True
+# edge = True
 visitedBool = False
 vistedPoint = Vector3( 0, 0, 0)
 refListPath = "/home/ros/Documents/my_test_project/refList/"
@@ -23,16 +23,18 @@ radii = 10
 degrees = 33
 rngTime =0.1
 filename = refListPath + fileName
+ravin = False
+
 #fileObject = open(filename, 'w')
 #global initialTime
 global n
 n = 1
 
-def vistedEgde(data):
-    global edge
-    edge = data.data
-    rospy.loginfo(rospy.get_caller_id() + "We have seen an edge %s", data.data)
-    rospy.spin()
+# def vistedEgde(data):
+#     global edge
+#     edge = data.data
+#     rospy.loginfo(rospy.get_caller_id() + "We have seen an edge %s", data.data)
+#     rospy.spin()
 
 def vistedCB(data):
     global visitedBool
@@ -46,7 +48,7 @@ def vistedPointCB(data):
 #    filename = refListPath + fileName
 
     #fileObject = open(filename, 'wb')
-    
+
     x_coord = data.x
     y_coord = data.y
     z_coord = data.z
@@ -61,27 +63,28 @@ def vistedPointCB(data):
     #fileObject.write(msgData)
     #spentSecs = time - initilaTime
     #fileObject.close()
-    
+
 def resetCallback(msg):
 	global reset
 	reset = msg.data
 	pass
-   
+
 def ravinCallback(msg):
 	global ravin
 	ravin = msg.data
 	pass
-    
+
+
 
 def setupFile(n):
     #model file
     #scale factor x,y,z,bool
     #serial number structured
     global refListPath, fileName, fileObject
-    
+
     fn = "/home/ros/Documents/my_test_project/refList/visited_point{}.txt".format(n)
     fileObject = open(fn, 'w')
-    
+
     filename = refListPath + fileName
     #fileObject = open(filename, 'wb')
     shipFile = "text\n"
@@ -105,44 +108,46 @@ def pol2cart(theta, r):
 
 #fixed angle
 def rndWalk():
-    global edge, radii, degrees, position, ravin
-    turnTheta = 90*math.pi/180
+    global radii, degrees, position, ravin
+    # turnTheta = 90*math.pi/180
     if ravin == True:
-        z,y = pol2cart(turnTheta,radii)
+        degrees += random.randint(140,220)
+        turnTheta = degrees * math.pi/180
+        x,y = pol2cart(turnTheta,radii)
         ravin = False
     else:
         degrees = degrees + 33
         turnTheta = degrees * math.pi /180
-        z,y = pol2cart(turnTheta,radii)
-        edge = False
-    z += position[2]
+        x,y = pol2cart(turnTheta,radii)
+        # edge = False
+    x += position[0]
     y += position[1]
-    return position[0],y,z
+    return x,y,position[2]
 
 #rng Turn
 def rndWalkRngTurn():
-    global theta, edge, rngTime, position, ravin
+    global theta, rngTime, position, ravin, degrees
     radii = 2#10
-    degrees  = 0
+    # degrees  = 0
     turnTheta = 0
-    z,y = 0, 0
+    x,y = 0, 0
     now = time.time()
     elapsed = now - rngTime
-    if (edge == True and elapsed >  60) or ravin == True:
-        degrees = degrees + random.randint(1,40)
+    if ravin: #(edge == True and elapsed >  60) or ravin == True:
+        degrees = degrees + random.randint(140,220)
         turnTheta = degrees * math.pi /180
-        z,y = pol2cart(turnTheta,radii)
-        edge = False
+        x,y = pol2cart(turnTheta,radii)
+        # edge = False
         rngTime = time.time()
         ravin = False
-        
+
 
     else:
-        z,y = pol2cart(turnTheta,radii)
-    z += position[2]
+        x,y = pol2cart(turnTheta,radii)
+    x += position[0]
     y += position[1]
-    return position[0],y,z
-    
+    return x,y,position[2]
+
 def reseting():
 	x,y,z = rndWalkRngTurn()
 	return x,y,z
@@ -159,13 +164,13 @@ def mainGlobalPlan():
     sleep = rospy.Rate(5)
     pub_goal = rospy.Publisher("goal", Vector3, queue_size=1)
     rospy.Subscriber("reachPoint", Bool, vistedCB)
-    rospy.Subscriber("reachEdge", Bool, vistedEgde)
+    # rospy.Subscriber("reachEdge", Bool, vistedEgde)
     rospy.Subscriber("path",Quaternion,vistedPointCB)
     rospy.Subscriber("reset",Bool,resetCallback)
     rospy.Subscriber("ravin",Bool,ravinCallback)
     finish_pub = rospy.Publisher("finish",Bool,queue_size=2)
     reset_pub = rospy.Publisher("reset",Bool,queue_size=2)
-    
+
     global t, fileObject, n, reset, position
     position = (0,0,0)
     #rospy.loginfo(type(initialTime))
@@ -179,7 +184,7 @@ def mainGlobalPlan():
     while not rospy.is_shutdown():
         global visitedBool
         initialTime = 0
-        
+
         if reset:
             x,y,z = reseting()
             reset_pub.publish(Bool(False))
@@ -190,7 +195,7 @@ def mainGlobalPlan():
             pub_goal.publish(goalPoint)
             egde = False
             sleep.sleep()
-        	
+
         #time = 0
         #follow ref. list
         data = False
@@ -241,8 +246,8 @@ def mainGlobalPlan():
 	                pub_goal.publish(goalPoint)
 	                egde = False
 	                sleep.sleep()
-	            
-	            #rospy.loginfo(time, initialTime)    
+
+	            #rospy.loginfo(time, initialTime)
 	            if initialTime == 0:
 	                #rospy.loginfo("in")
 	                initialTime = t
@@ -261,7 +266,7 @@ def mainGlobalPlan():
     #			n += 1
     #			break"""
         #rospy.loginfo(time)
-        if t  >= 5*60:#3600*24:
+        if t  >= 3600*7:
             # rospy.loginfo("ok")
             bool_pub = Bool()
             bool_pub.data = True
@@ -273,9 +278,9 @@ def mainGlobalPlan():
             #fileObject.close()
             #setupFile(n)
             #break
-            
+
         #counter += 1
-		
+
         pass
 
     # spin() simply keeps python from exiting until this node is stopped
